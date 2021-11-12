a1341c4875e5abc63ed6d90f5d2e64f194a183f9
Sampath1296 <94166514+Sampath1296@users.noreply.github.com>
2021-11-12 21:52:15 +0530

    second commit

--- code.R
+++ code.R
@@ -1,24 +1,32 @@
+#Package installation
 install.packages("dplyr", dependencies=TRUE, INSTALL_opts = c('--no-lock'))
 library(dplyr)
+
+# Data loading
 data <- read.csv("cyber-security-1_question-response.csv")
 head(data)
 str(data)
+
+# Dropping unnecessary columns
 data[,c("cloze_response","question_type","submitted_at")] <- list(NULL)
 colnames(data)
 
+# How many unique elements are present in each column
 for(i in colnames(data)){
   print(sum(!duplicated(data[i])))
 }
 
+# How many empty cells are present in each column
 for(i in colnames(data)){
   print(sum(data[i]==""))
 }
 
+# Dropping empty cells
 data[data==""]<-NA
 data<-data[complete.cases(data),]
 sum(data=="")
 
-# which question is mostly correct
+# which question is mostly correct in between all quizzes
 data %>%
   group_by(question_number, correct) %>%
   summarise(a_sum=sum(correct=="true",
@@ -29,7 +37,7 @@ data %>%
 paste("Total number of corrected question:",p=sum(data$correct=="true"))
 paste("Total number of incorrected question:",p=sum(data$correct=="false"))
 
-# Top 10 high scorers
+# Top 10 high scorers (learning_id)
 df1<- data.frame(data)
 df2<-subset(df1, correct=="true")
 z<-df2 %>%
@@ -40,8 +48,28 @@ z[order(-z$true_sum),]
 
 # Which quiz question is most correctly marked
 df1<- data.frame(data)
-df2<-subset(df1, correct=="false")
-data %>%
+df2<-subset(df1, correct=="true")
+z<-df2 %>%
   group_by(quiz_question, correct) %>%
   summarise(true_sum=sum(correct=="true",
                       correct=="false"))
+z[order(-z$true_sum),]
+
+# Which quiz question is most incorrectly marked
+df1<- data.frame(data)
+df2<-subset(df1, correct=="false")
+z<-df2 %>%
+  group_by(quiz_question, correct) %>%
+  summarise(true_sum=sum(correct=="true",
+                         correct=="false"))
+z[order(-z$true_sum),]
+
+# which step number got the most correctly marked
+df1<- data.frame(data)
+df2<-subset(df1, correct=="true")
+z<-df2 %>%
+  group_by(step_number, correct) %>%
+  summarise(true_sum=sum(correct=="true",
+                         correct=="false"))
+z[order(-z$true_sum),]
+
