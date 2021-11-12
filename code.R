ee45506237d9c8d81d7e3c4d0fcd9f1e06c263a2
Sampath1296 <94166514+Sampath1296@users.noreply.github.com>
2021-11-12 22:42:05 +0530

    third commit

--- code.R
+++ code.R
@@ -4,7 +4,9 @@ library(dplyr)
 
 # Data loading
 data <- read.csv("cyber-security-1_question-response.csv")
-head(data)
+head(data,10)
+tail(data,10)
+dim(data)
 str(data)
 
 # Dropping unnecessary columns
@@ -26,6 +28,7 @@ data[data==""]<-NA
 data<-data[complete.cases(data),]
 sum(data=="")
 
+
 # which question is mostly correct in between all quizzes
 data %>%
   group_by(question_number, correct) %>%
@@ -33,9 +36,18 @@ data %>%
                       correct=="false"))
 # question 1 is mostly correct
 
+
+
 # Total number of Corrected questions and incorrected questions
 paste("Total number of corrected question:",p=sum(data$correct=="true"))
 paste("Total number of incorrected question:",p=sum(data$correct=="false"))
+H <- c(sum(data$correct=="true"),sum(data$correct=="false"))
+M <- c("corrected","incorrected")
+# Plot the bar chart 
+barplot(H,names.arg=M,xlab="",ylab="",col="brown",
+        main="Total number of Corrected questions and incorrected questions",border="red")
+
+
 
 # Top 10 high scorers (learning_id)
 df1<- data.frame(data)
@@ -46,6 +58,9 @@ z<-df2 %>%
                       correct=="false"))
 z[order(-z$true_sum),]
 
+
+
+
 # Which quiz question is most correctly marked
 df1<- data.frame(data)
 df2<-subset(df1, correct=="true")
@@ -54,6 +69,12 @@ z<-df2 %>%
   summarise(true_sum=sum(correct=="true",
                       correct=="false"))
 z[order(-z$true_sum),]
+# Plot the bar chart 
+barplot(z$true_sum,names.arg=z$quiz_question,las=2,xlab="",ylab="",col="green",
+        main="Quiz question correctly marked",border="black")
+
+
+
 
 # Which quiz question is most incorrectly marked
 df1<- data.frame(data)
@@ -63,6 +84,12 @@ z<-df2 %>%
   summarise(true_sum=sum(correct=="true",
                          correct=="false"))
 z[order(-z$true_sum),]
+# Plot the bar chart 
+barplot(z$true_sum,names.arg=z$quiz_question,las=2,xlab="",ylab="",col="red",
+        main="Quiz question incorrectly marked",border="black")
+
+
+
 
 # which step number got the most correctly marked
 df1<- data.frame(data)
@@ -72,4 +99,22 @@ z<-df2 %>%
   summarise(true_sum=sum(correct=="true",
                          correct=="false"))
 z[order(-z$true_sum),]
+# Plot the bar chart 
+barplot(z$true_sum,names.arg=z$step_number,xlab="",ylab="",col="green",
+        main="Total step numbers which are correctly marked",border="black")
+
+
+
+
+# which step number got the most incorrectly marked
+df1<- data.frame(data)
+df2<-subset(df1, correct=="false")
+z<-df2 %>%
+  group_by(step_number, correct) %>%
+  summarise(true_sum=sum(correct=="true",
+                         correct=="false"))
+z[order(-z$true_sum),]
+# Plot the bar chart 
+barplot(z$true_sum,names.arg=z$step_number,xlab="",ylab="",col="red",
+        main="Total step numbers which are incorrectly marked",border="black")
 
